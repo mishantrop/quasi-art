f8015b20e19ed797c42231e5ae569bac9f14022a
mishantrop <quasi-art@ya.ru>
2020-07-18 17:59:26 +0400

    Update dependencies, relocate dist files

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 const gulp = require('gulp');
-const autoprefixer = require('autoprefixer');
+// const autoprefixer = require('autoprefixer');
 const cssnano = require('gulp-cssnano');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
+// const sourcemaps = require('gulp-sourcemaps');
 
 const templateMainPath = 'assets/templates/main/';
-const distMainPath = 'assets/templates/main/';
+const distMainPath = 'assets/templates/main/dist/';
 
 gulp.task('postcss-main', function() {
   return gulp.src(templateMainPath + 'postcss/main.css')
     .pipe(postcss())
     .pipe(rename('main.min.css'))
     .pipe(cssnano())
-    .pipe(gulp.dest(distMainPath + 'css'));
+    .pipe(gulp.dest(distMainPath));
 });
 
-gulp.task('serve', ['postcss-main'], function() {
-    gulp.watch(templateMainPath + 'postcss/*.css', ['postcss-main']);
+gulp.task('serve', [ 'postcss-main' ], function() {
+  gulp.watch(templateMainPath + 'postcss/*.css', [ 'postcss-main' ]);
 });
 
-gulp.task('watch-main', ['postcss-main'], function() {
-    gulp.watch(templateMainPath + 'postcss/*.css', ['postcss-main']);
+gulp.task('watch-main', [ 'postcss-main' ], function() {
+  gulp.watch(templateMainPath + 'postcss/*.css', [ 'postcss-main' ]);
 });
 
-gulp.task('default', ['serve']);
-gulp.task('watch', ['watch-main']);
+gulp.task('default', [ 'serve' ]);
+gulp.task('watch', [ 'watch-main' ]);
--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,56 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-  entry: path.resolve(__dirname, 'assets/templates/main/js/src/index.js'),
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'assets/templates/main/js/dist'),
-    filename: 'main.min.js',
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      query: {
-        plugins: [
-          'babel-plugin-transform-class-properties',
-        ],
-        presets: [['env', {
-          targets: {
-            browsers: [
-              'last 2 versions',
-              'safari >= 7',
-            ],
-          },
-          include: [
-            'transform-es2015-arrow-functions',
-            // 'transform-class-properties',
-            // 'transform-flow-strip-types',
-            'es6.map',
+module.exports = env => {
+  const config = {
+    entry: path.resolve(__dirname, 'assets/templates/main/js/src/index.js'),
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, 'assets/templates/main/dist'),
+      filename: 'main.min.js',
+    },
+    module: {
+      loaders: [ {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          plugins: [
+            'babel-plugin-transform-class-properties',
           ],
-          modules: false,
-          useBuiltIns: 'entry',
-        }]],
-      },
-    }],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': 'production',
-    }),
-    new UglifyJSPlugin(),
-  ],
-  stats: {
-    colors: true,
-  },
-  devtool: 'source-map',
+          presets: [
+            [ 'env', {
+              targets: {
+                browsers: [
+                  'last 2 versions',
+                  'safari >= 7',
+                ],
+              },
+              include: [
+                'transform-es2015-arrow-functions',
+                'es6.map',
+              ],
+              modules: false,
+              useBuiltIns: 'entry',
+            } ],
+          ],
+        },
+      } ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': 'production',
+      }),
+      new UglifyJSPlugin(),
+    ],
+    stats: {
+      colors: true,
+    },
+    devtool: 'source-map',
+  };
+
+  if (env && env.ANALYZE === 1) {
+    config.plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return config;
 };
