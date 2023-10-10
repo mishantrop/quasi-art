361bc6c9e201598b9c92721d419a42198b944900
mishantrop <quasi-art@yandex.ru>
2023-10-10 21:00:50 +0300

    css/js/thumbs

--- assets/templates/main/js/src/backtotop.ts
+++ assets/templates/main/js/src/backtotop.ts
@@ -9,28 +9,31 @@ export const initBackToTopScroll = ({
     const backToTopClassActive = 'back-to-top--active'
     const scrollTrigger = 300
     const backToTop = document.querySelector(backToTopSelector)
-    if (backToTop) {
+    if (!backToTop) {
+        console.warn('backToTop not found')
+        return
+    }
+
+    if (window.scrollY > scrollTrigger) {
+        backToTop.classList.add(backToTopClassActive)
+    } else {
+        backToTop.classList.remove(backToTopClassActive)
+    }
+
+    window.addEventListener('scroll', () => {
         if (window.scrollY > scrollTrigger) {
             backToTop.classList.add(backToTopClassActive)
         } else {
             backToTop.classList.remove(backToTopClassActive)
         }
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > scrollTrigger) {
-                backToTop.classList.add(backToTopClassActive)
-            } else {
-                backToTop.classList.remove(backToTopClassActive)
-            }
-        })
-        const el = document.querySelector(targetSelector) || false
-        if (el) {
-            backToTop.addEventListener('click', () => {
-                scrollToElement(targetSelector)
-            })
-        } else {
-            console.warn('target not found: ', targetSelector)
-        }
-    } else {
-        console.warn('backToTop not found')
+    })
+
+    const el = document.querySelector(targetSelector) || false
+    if (el) {
+        console.warn('target not found: ', targetSelector)
+        return
     }
+    backToTop.addEventListener('click', () => {
+        scrollToElement(targetSelector)
+    })
 }
